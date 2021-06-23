4ed1b47e0d6b9d131077a8e978958a17a2d233bf
geekfly <wangxxx51@gmail.com>
2021-06-23 18:19:51 +0800

    小修改

--- 02-canvas-drawing-board/main.js
+++ 02-canvas-drawing-board/main.js
@@ -1,4 +1,4 @@
-let canvas = document.querySelector('canvas')
+let canvas = document.querySelector('#canvas')
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
 let ctx = canvas.getContext('2d')
