8bc5b2576a0b478086c9b9fe67c5590c0be9dfc7
geekfly <wangxxx51@gmail.com>
2021-07-04 22:46:22 +0800

    基本完成

--- 03-dynamic-code/main.js
+++ 03-dynamic-code/main.js
@@ -6,22 +6,70 @@ let n = -1
 
 let string = `/*
 * 你好，我是一名前端新人
-* 接下来我要加样式了
-* 我要加的样式是
-*/
-body{
-    color: red;
-}
-/*
-* 然后我要准备一个div
+* 接下来我要展示一下我的前端功底
+* 首先要准备一个div
 */
 #div1{
-    position: fixed;
-    right: 20px;
-    top: 20px;
     border: 1px solid red;
-    width: 400px;
-    height: 400px
+    width: 200px;
+    height: 200px
+}
+/*
+* 接着把div变成一个圆
+*/
+#div1{
+    border-radius: 50%;
+    border: none;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+}
+/*
+* 八卦分阴阳两极
+* 一黑一白
+*/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%);
+}
+/*
+* 加两个神秘的小球
+* 一黑一白
+*/
+#div1::before{
+    content: '';
+    width: 100px;
+    height: 100px;
+    background: radial-gradient(circle, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%);
+    position: relative;
+    display: block;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 50%;
+}
+#div1::after{
+    content: '';
+    width: 100px;
+    height: 100px;
+    background: radial-gradient(circle, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%);
+    position: relative;
+    display: block;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 50%;
+}
+/*
+* 我想让这个八卦旋转起来
+*/
+@keyframes spin {
+    from{
+        transform: rotate(0deg);
+    }
+    to{
+        transform: rotate(360deg);
+    }
+}
+#div1{
+    animation: spin 3s linear infinite;
 }
 `
 
@@ -39,10 +87,11 @@ let step = () => {
         }
         html.innerHTML = string2
         style.innerHTML = string.substring(0, n)
+        window.scroll(0, 99999)
         if (n < string.length - 1) {
             step()
         }
-    }, 100)
+    }, 0)
 }
 
 step()
